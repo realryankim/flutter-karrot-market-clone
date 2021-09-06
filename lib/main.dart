af48b9ea888ac0f4457ad8bea39e8078d7c25763
realryankim <ryankimofficial@gmail.com>
2021-09-06 11:49:05 +0900

    initial setting

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Carrot Market Clone',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Container(),
+    );
+  }
+}
